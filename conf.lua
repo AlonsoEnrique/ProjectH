d3c2860fc173723fe5a7633fd0b5663a776c38d1
Alonso <alons45@gmail.com>
2018-04-04 16:56:57 -0500

    Add new name to title and resizable=true

--- conf.lua
+++ conf.lua
@@ -1,5 +1,7 @@
 function love.conf (t)
     t.window.width = 800
     t.window.height = 600
-    t.title = 'Project H'
+    t.title = 'Save Earth'
+    t.window.resizable = true
+--    t.window.fullscreen = true
 end
