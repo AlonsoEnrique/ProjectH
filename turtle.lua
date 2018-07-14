f700c9fa5bc4609f6e078d4c88c9b0ee25b2b112
Alonso <alons45@gmail.com>
2018-07-14 17:38:42 -0500

    add pseudocode to turtle

--- turtle.lua
+++ turtle.lua
@@ -2,6 +2,11 @@ Turtle = {}
 Turtle.__index = Turtle
 
 function Turtle:new ()
+    """ Turtle
+    speed: POSITIVE INTEGER
+    body
+    sound_when_swim
+    """
     self = setmetatable ({}, Turtle)
 
     self.radius = 20
@@ -37,6 +42,20 @@ function Turtle:new ()
             self.y = self.y + self.velocity * dt
         end
     end
+
+    """
+    Algorithm: swim
+    START
+        IF player move to LEFT
+            swim to left
+        ELSE IF player move to RIGHT
+            swim to right
+        ELSE IF player move to up
+            swim to up
+        ELSE
+            swim to down
+    END
+    """
 
     -- TODO: best way for this?
     function self.body ()
