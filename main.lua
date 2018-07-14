68ff2bbf75a495525b175b6a9465d3f0bfdfe261
Alonso <alons45@gmail.com>
2018-07-14 17:41:50 -0500

    add pseudocode to main

--- main.lua
+++ main.lua
@@ -2,6 +2,13 @@ require 'turtle'
 require 'diver'
 
 function love.load ()
+    """ Sea
+    turtle_population: POSITIVE INTEGER
+    turtle: Turtle OBJECT
+    divers: Diver OBJECT LIST
+    is_enough_dives_hunting: BOOLEAN
+    natural_sounds: STRING
+    """
     math.randomseed (os.time ())
 
     turtle = Turtle:new ()
@@ -24,6 +31,16 @@ function love.draw ()
 end
 
 function love.update (deltatime)
+    """
+    Algorithm: update
+    Description: This algorithm is a loop when the main actions happen
+    START
+        game_over()
+        won()
+        enough_hunting_dives()
+        turtle.movement()
+    END
+    """
     turtle:update (deltatime)
 
     if timeToGenerateDiver == 0 then
@@ -41,3 +58,32 @@ function love.update (deltatime)
         diver:update (deltatime)
     end
 end
+
+
+"""
+Algorithm: game_over
+START
+    IF turtle is touch for divers
+        RETURN True
+    ELSE
+        RETURN False
+END
+"""
+
+"""
+Algorithm: won
+START
+    IF turtle reach final
+        RETURN True
+    ELSE
+        RETURN False
+END
+"""
+
+"""
+Algorithm: enough_dives_hunting
+START
+    IF exists enough dives hunting (enough_dives_hunting) the turtle
+        GENERATE dives
+END
+"""
