62a94a26dbd91d2a33c2fe082c2293248550ccee
Alonso <alons45@gmail.com>
2018-07-16 16:55:28 -0500

    min changes

--- turtle.lua
+++ turtle.lua
@@ -2,19 +2,15 @@ Turtle = {}
 Turtle.__index = Turtle
 
 function Turtle:new ()
-    --[[ Turtle
-    speed: POSITIVE INTEGER
-    body
-    sound_when_swim
-    ]]
     self = setmetatable ({}, Turtle)
 
-    self.radius = 20
-    self.x = love.graphics.getWidth () / 2
-    self.y = love.graphics.getHeight () - self.radius
-
-    self.speed = 300
+    self.speed = 200
+    self.body = null
     self.soundWhenSwim = null
+
+    self.radius = 30
+    self.x_position = love.graphics.getWidth () / 2
+    self.y_position = love.graphics.getHeight () - self.radius
 
     function self.sayHello ()
         print ('Hi, i\'m the Turtle, take care!')
@@ -22,25 +18,25 @@ function Turtle:new ()
 
     function self.move_to_left (deltaTime)
         if love.keyboard.isDown ('left') then
-            self.x = self.x - self.speed * deltaTime
+            self.x_position = self.x_position - self.speed * deltaTime
         end
     end
 
     function self.move_to_right (deltaTime)
         if love.keyboard.isDown ('right') then
-            self.x = self.x + self.speed * deltaTime
+            self.x_position = self.x_position + self.speed * deltaTime
         end
     end
 
     function self.move_to_up (deltaTime)
         if love.keyboard.isDown ('up') then
-            self.y = self.y - self.speed * deltaTime
+            self.y_position = self.y_position - self.speed * deltaTime
         end
     end
 
     function self.move_to_down (deltaTime)
         if love.keyboard.isDown ('down') then
-            self.y = self.y + self.speed * deltaTime
+            self.y_position = self.y_position + self.speed * deltaTime
         end
     end
 
@@ -50,23 +46,10 @@ function Turtle:new ()
         self.move_to_down (deltaTime)
         self.move_to_up (deltaTime)
     end
-    --[[
-    Algorithm: swim
-    START
-        IF player move to LEFT
-            swim to left
-        ELSE IF player move to RIGHT
-            swim to right
-        ELSE IF player move to up
-            swim to up
-        ELSE
-            swim to down
-    END
-    ]]
 
     -- TODO: best way for this?
     function self.body ()
-        love.graphics.circle('line', self.x, self.y, self.radius)
+        love.graphics.circle('line', self.x_position, self.y_position, self.radius)
     end
 
     return self
