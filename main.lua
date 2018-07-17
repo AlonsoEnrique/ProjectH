1e18350b87014765f0427cb33be26d5cc9e03b16
Alonso <alons45@gmail.com>
2018-07-17 11:58:45 -0500

    implement more pseudocode

--- main.lua
+++ main.lua
@@ -2,32 +2,25 @@ require 'turtle'
 require 'diver'
 
 function love.load ()
-    --[[ Sea
-    turtle_population: POSITIVE INTEGER
-    turtle: Turtle OBJECT
-    divers: Diver OBJECT LIST
-    is_enough_dives_hunting: BOOLEAN
-    natural_sounds: STRING
-    ]]
     math.randomseed (os.time ())
-
+    
+    turtlePopulation = null -- POSITIVE INTEGER
     turtle = Turtle:new ()
-    turtle.sayHello ()
-
-    diverList = {}
-
+    divers = {} -- LIST
+    isEnoughDivesHunting = null -- BOOLEAN
     timeToGenerateDiver = 20
+
     background_music = love.audio.newSource('sounds/mainmenu.mp3', 'stream')
     background_music:setLooping(true)
-    background_music:play(background_music)
+    -- background_music:play(background_music)
 end
 
 function love.draw ()
     turtle:draw()
 
---[[     for index, diver in ipairs (diverList) do
+    for index, diver in ipairs (divers) do
         diver:draw ()
-    end ]]
+    end
 end
 
 function love.update (deltatime)
@@ -46,7 +39,7 @@ function love.update (deltatime)
     if timeToGenerateDiver == 0 then
         local xPosition = math.random (1, love.graphics.getWidth ())
         local yPosition = math.random (1, love.graphics.getHeight () / 2)
-        table.insert (diverList, Diver:new (xPosition, yPosition))
+        table.insert (divers, Diver:new (xPosition, yPosition))
 
         timeToGenerateDiver = 20
 
@@ -54,7 +47,7 @@ function love.update (deltatime)
         timeToGenerateDiver = timeToGenerateDiver - 1
     end
 
-    for index, diver in ipairs (diverList) do
+    for index, diver in ipairs (divers) do
         diver:update (deltatime)
     end
 end
