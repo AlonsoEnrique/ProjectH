f376df761c68cbb47d45762acc9cfbe08d0ffe65
Alonso <alons45@gmail.com>
2018-07-17 13:02:07 -0500

    fix programming style in variables

--- turtle.lua
+++ turtle.lua
@@ -9,8 +9,8 @@ function Turtle:new ()
     self.soundWhenSwim = null
 
     self.radius = 30
-    self.x_position = love.graphics.getWidth () / 2
-    self.y_position = love.graphics.getHeight () - self.radius
+    self.xPosition = love.graphics.getWidth () / 2
+    self.yPosition = love.graphics.getHeight () - self.radius
 
     function self.sayHello ()
         love.graphics.print('Hi, i\'m the Turtle, take care!', 0, 0)
@@ -18,32 +18,32 @@ function Turtle:new ()
 
     function self.move_to_left (deltaTime)
         if love.keyboard.isDown ('left') then
-            if self.x_position >= 0 then
-                self.x_position = self.x_position - self.speed * deltaTime
+            if self.xPosition >= 0 then
+                self.xPosition = self.xPosition - self.speed * deltaTime
             end
         end
     end
 
     function self.move_to_right (deltaTime)
         if love.keyboard.isDown ('right') then
-            if self.x_position <= love.graphics.getWidth () then
-                self.x_position = self.x_position + self.speed * deltaTime
+            if self.xPosition <= love.graphics.getWidth () then
+                self.xPosition = self.xPosition + self.speed * deltaTime
             end
         end
     end
 
     function self.move_to_up (deltaTime)
         if love.keyboard.isDown ('up') then
-            if self.y_position >= 0 then
-                self.y_position = self.y_position - self.speed * deltaTime
+            if self.yPosition >= 0 then
+                self.yPosition = self.yPosition - self.speed * deltaTime
             end
         end
     end
 
     function self.move_to_down (deltaTime)
         if love.keyboard.isDown ('down') then
-            if self.y_position <= love.graphics.getHeight () then
-                self.y_position = self.y_position + self.speed * deltaTime
+            if self.yPosition <= love.graphics.getHeight () then
+                self.yPosition = self.yPosition + self.speed * deltaTime
             end    
         end
     end
@@ -63,7 +63,7 @@ function Turtle:load ()
 end
 
 function Turtle:draw ()
-    love.graphics.circle('line', self.x_position, self.y_position, self.radius)
+    love.graphics.circle('line', self.xPosition, self.yPosition, self.radius)
     self.sayHello ()
 end
 
